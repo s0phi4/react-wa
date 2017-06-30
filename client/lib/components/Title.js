8d6776379e0a0ec335a2b2064aec474f862289bc
yenpraja610 <yen.a@me.com>
2017-06-30 13:07:43 -0400

    Initialization of repo

---
+++ client/lib/components/Title.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Title extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+
+    return(
+      <h1>Testing</h1>
+    )
+
+  }
+}
+
+export default Title;
